fb0c7d08b2906bb38a2394eb80bd3e309a7bae1c
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-04-14 20:06:25 +0200

    Tune build pipeline

--- InstallPackagesUbuntu.sh
+++ InstallPackagesUbuntu.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
+
 readonly ubuntu_release="$(grep "DISTRIB_RELEASE" /etc/lsb-release | sed -e 's/DISTRIB_RELEASE=//')"
 if [[ "${ubuntu_release}" == "22.04" ]]; then
-  apt-get install -qq \
+  sudo apt-get install -qq \
     g++ cmake \
     libeigen3-dev freeglut3-dev libopencv-dev \
     qtdeclarative5-dev libqt5multimedia5 \
@@ -10,7 +12,7 @@ if [[ "${ubuntu_release}" == "22.04" ]]; then
     libdc1394-25 libdc1394-dev \
     libv4l-0
 elif [[ "${ubuntu_release}" == "20.04" ]]; then
-  apt-get install -qq \
+  sudo apt-get install -qq \
     g++ cmake \
     libeigen3-dev freeglut3-dev libopencv-dev \
     qtdeclarative5-dev libqt5multimedia5 \
