e793bed4be7ee35b91bf043a67f088791c88e1f6
Nicolai Ommer <nicolai.ommer@gmail.com>
2023-04-15 15:09:54 +0200

    Update Spinnaker capture module
    - Remove obsolete or static parameters
    - Add image ROI parameters
    - Add button that resets camera to default parameters
    - Set VarType min/max values from camera
    - Allow limiting the frame rate on the camera

--- src/shared/capture/capture_spinnaker.h
+++ src/shared/capture/capture_spinnaker.h
@@ -53,22 +53,25 @@ Q_OBJECT
 
 public slots:
 
-    void changed(VarTypes::VarType *group);
+    void changed(__attribute__((unused)) VarType *group);
 
-protected:
-    QMutex mutex;
+    void slotResetTriggered();
 
-public:
-
-protected:
+private:
+    unsigned int cam_id;
     bool is_capturing;
+    bool reset_parameters = false;
+
+    QMutex mutex;
     TimeSync timeSync;
 
-    //capture variables:
+    // capture parameters
     VarInt *v_cam_bus;
     VarStringEnum *v_convert_to_mode;
+    VarBool *v_use_camera_time;
 
-    //DCAM parameters:
+    // camera parameters
+    VarBool *v_acquisition;
     VarStringEnum *v_capture_mode;
     VarStringEnum *v_expose_auto;
     VarDouble *v_expose_us;
@@ -79,11 +82,14 @@ protected:
     VarStringEnum *v_white_balance_auto;
     VarDouble *v_white_balance_red;
     VarDouble *v_white_balance_blue;
-    VarStringEnum *v_stream_buffer_handling_mode;
-    VarInt *v_stream_buffer_count;
-    VarBool *v_use_camera_time;
+    VarInt *v_image_width;
+    VarInt *v_image_height;
+    VarInt *v_image_offset_x;
+    VarInt *v_image_offset_y;
+    VarBool *v_frame_rate_enable;
     VarDouble *v_frame_rate;
     VarDouble *v_frame_rate_result;
+    VarTrigger *v_trigger_reset;
 
     VarList *capture_settings;
     VarList *dcam_parameters;
@@ -93,45 +99,39 @@ protected:
     Spinnaker::CameraPtr pCam;
     Spinnaker::ImagePtr pImage;
 
-    unsigned int cam_id;
-
 public:
     explicit CaptureSpinnaker(VarList *_settings = nullptr, int default_camera_id = 0, QObject *parent = nullptr);
 
-    void mvc_connect(VarList *group);
-
     ~CaptureSpinnaker() override;
 
-    /// Initialize the interface and start capture
     bool startCapture() override;
 
-    /// Stop Capture
     bool stopCapture() override;
 
     bool isCapturing() override { return is_capturing; };
 
-    /// this gives a raw-image with a pointer directly to the video-buffer
-    /// Note that this pointer is only guaranteed to point to a valid
-    /// memory location until releaseFrame() is called.
     RawImage getFrame() override;
 
     void releaseFrame() override;
 
-    bool resetBus() override;
-
-    void readParameterValues(VarList *item);
-
-    void writeParameterValues(VarList *item);
+    bool resetBus() override { return true; };
 
     void readAllParameterValues() override;
 
-    void writeAllParameterValues();
-
     bool copyAndConvertFrame(const RawImage &src, RawImage &target) override;
 
-    string getCaptureMethodName() const override;
+    string getCaptureMethodName() const override { return "Spinnaker"; };
 
 private:
+    void mvc_connect(VarList *group);
+
+    void readParameterValues();
+
+    void writeParameterValues();
+
+    void reloadParameters();
+
+    void init_camera();
 
     static string toString(Spinnaker::ExposureAutoEnums e) {
       switch (e) {
@@ -207,33 +207,62 @@ private:
       return Spinnaker::BalanceWhiteAuto_Off;
     }
 
-
-    static string toString(Spinnaker::StreamBufferHandlingModeEnum e) {
-      switch (e) {
-        case Spinnaker::StreamBufferHandlingMode_NewestOnly:
-          return "NewestOnly";
-        case Spinnaker::StreamBufferHandlingMode_NewestFirst:
-          return "NewestFirst";
-        case Spinnaker::StreamBufferHandlingMode_OldestFirstOverwrite:
-          return "OldestFirstOverwrite";
-        case Spinnaker::StreamBufferHandlingMode_OldestFirst:
-          return "OldestFirst";
-        default:
-          return "OldestFirstOverwrite";
+    static void setCameraValueInt(Spinnaker::GenApi::IInteger &cameraValue, VarInt *varValue) {
+      if (IsWritable(cameraValue)) {
+        varValue->setMin((int) cameraValue.GetMin());
+        varValue->setMax((int) cameraValue.GetMax());
+        cameraValue.SetValue(cap((int) varValue->getInt(), varValue->getMin(), varValue->getMax()));
+        varValue->removeFlags(VARTYPE_FLAG_READONLY);
+      } else {
+        varValue->addFlags(VARTYPE_FLAG_READONLY);
       }
     }
 
-    static Spinnaker::StreamBufferHandlingModeEnum stringToStreamBufferHandlingMode(const char *s) {
-      if (strcmp(s, "NewestOnly") == 0) {
-        return Spinnaker::StreamBufferHandlingMode_NewestOnly;
-      } else if (strcmp(s, "NewestFirst") == 0) {
-        return Spinnaker::StreamBufferHandlingMode_NewestFirst;
-      } else if (strcmp(s, "OldestFirstOverwrite") == 0) {
-        return Spinnaker::StreamBufferHandlingMode_OldestFirstOverwrite;
-      } else if (strcmp(s, "OldestFirst") == 0) {
-        return Spinnaker::StreamBufferHandlingMode_OldestFirst;
+    static void setCameraValueFloat(Spinnaker::GenApi::IFloat &cameraValue, VarDouble *varValue) {
+      if (IsWritable(cameraValue)) {
+        varValue->setMin(cameraValue.GetMin());
+        varValue->setMax(cameraValue.GetMax());
+        cameraValue.SetValue(cap(varValue->getDouble(), varValue->getMin(), varValue->getMax()));
+        varValue->removeFlags(VARTYPE_FLAG_READONLY);
+      } else {
+        varValue->addFlags(VARTYPE_FLAG_READONLY);
       }
-      return Spinnaker::StreamBufferHandlingMode_OldestFirstOverwrite;
+    }
+
+    static void getCameraValueInt(Spinnaker::GenApi::IInteger &cameraValue, VarInt *varValue) {
+      if (IsReadable(cameraValue)) {
+        varValue->setMin((int) cameraValue.GetMin());
+        varValue->setMax((int) cameraValue.GetMax());
+        varValue->setInt((int) cameraValue.GetValue());
+      }
+
+      if (IsWritable(cameraValue)) {
+        varValue->removeFlags(VARTYPE_FLAG_READONLY);
+      } else {
+        varValue->addFlags(VARTYPE_FLAG_READONLY);
+      }
+    }
+
+    static void getCameraValueFloat(Spinnaker::GenApi::IFloat &cameraValue, VarDouble *varValue) {
+      if (IsReadable(cameraValue)) {
+        varValue->setMin(cameraValue.GetMin());
+        varValue->setMax(cameraValue.GetMax());
+        varValue->setDouble(cameraValue.GetValue());
+      }
+
+      if (IsWritable(cameraValue)) {
+        varValue->removeFlags(VARTYPE_FLAG_READONLY);
+      } else {
+        varValue->addFlags(VARTYPE_FLAG_READONLY);
+      }
+    }
+
+    static double cap(double value, double v_min, double v_max) {
+      return max(v_min, min(v_max, value));
+    }
+
+    static int cap(int value, int v_min, int v_max) {
+      return max(v_min, min(v_max, value));
     }
 };
 
