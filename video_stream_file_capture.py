dfb3cf638feaa03627206db6eeeb4c95bd13198c
jakos322 <jakos322@student.liu.se>
2025-01-25 15:29:57 +0100

    Add new calibration
    Co-authored-by: haholm <57066006+haholm@users.noreply.github.com>

--- video_stream_file_capture.py
+++ video_stream_file_capture.py
@@ -42,14 +42,18 @@ try:
             if a != -1 and b != -1:
                 jpg = frame[a:b + 2]
                 frame = frame[b + 2:]
-                # img = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-                encoded_img = base64.b64encode(jpg).decode('utf-8')
-                with open(f"{script_dir}/camera{camera_number}.txt", "w") as file:
-                    file.write(encoded_img)
                 # Display the MJPEG video stream
-                # cv2.imshow("MJPEG Stream", img)
-                # if cv2.waitKey(1) & 0xFF == ord('q'):
-                #     break
+                img = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                if img is not None:
+                    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                    _, jpg = cv2.imencode('.jpg', img_rgb)
+                    encoded_img = base64.b64encode(jpg).decode('utf-8')
+
+                    with open(f"{script_dir}/camera{camera_number}.txt", "w") as file:
+                        file.write(encoded_img)
+                    # cv2.imshow("MJPEG Stream", img_rgb)
+                    # if cv2.waitKey(1) & 0xFF == ord('q'):
+                    #     break
 
 finally:
     # Release resources
